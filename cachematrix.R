2cb351e240bbde5532b052e32dc0ed61725ce5d5
maryli <mmllee@gmail.com>
2015-10-24 23:03:31 -0700

    add detailed comments to the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,13 @@
 ## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+  inv <- NULL # initialize inverse to NULL
   set <- function(y) {
-    x <<- y
+    x <<- y #this cache operation stores the data 
     inv <<- NULL
   }
-  get <- function() x
-  setinv <- function(x) inv <<- x
-  getinv <- function() inv
+  get <- function() x  # get function returns the matrix
+  setinv <- function(x) inv <<- x # sets the inverse 
+  getinv <- function() inv  # gets the inverse
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
@@ -20,23 +20,23 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
-  if(!is.null(inv)) {
+  if(!is.null(inv)) {  # if the inverse already exists, return it
     message("getting cached data")
     return(inv)
   }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinv(inv)
+  data <- x$get() # get the matrix
+  inv <- solve(data, ...) # solve the inverse of the matrix
+  x$setinv(inv)  # set the inverse of the matrix
   inv
 }
 
 ## test case
 # construct a 3x3 invertible matrix
-mdat <- matrix(c(1,2,3, 5,6,8, 11,12,13), nrow = 3, ncol = 3, byrow = TRUE,
-                dimnames = list(c("row1", "row2", "row3"),
-                c("C.1", "C.2", "C.3")))
+mat <- matrix(c(1,2,3, 5,6,8, 11,12,13), nrow = 3, ncol = 3, byrow = TRUE,
+                dimnames = list(c("A", "B", "C"),
+                c("r1", "r2", "r3")))
 # call the function to store the mdat matrix
-X <- makeCacheMatrix(mdat)
+X <- makeCacheMatrix(mat)
 # call cacheSolve function to get the inverse of the matix
 # first time, it solves it, after that returns the cached data
 cacheSolve(X)
